527b6af4223120f2f3bed5d2338f4366dc2c05c3
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-05 04:01:32 +0200

    added some text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,30 +25,32 @@
 
 	<div class="container">
 		<div class="text-center">
-			<img src="{{asset("img/logo.png")}}" alt="Marathonradio">
+			<img src="{{asset("img/logo.png")}}" alt="Marathonradio" id="logo">
+			<p>De onofficiële #marathonradio statistieken!</p>
+			<small>Teller loop sinds 5 juni 2016 en vernieuwt elke minuut.</small>
 		</div>
 		<div class="row" style="margin-top:20px;">
-			<div class="col-md-4">
+			<div class="col-md-4 text-center">
 				<div class="well">
 					<h1>{{$totalTweetCount}}</h1>
 					<h3>tweets met #marathonradio</h3>
 				</div>
 			</div>
 
-			<div class="col-md-4 col-md-push-4">
+			<div class="col-md-4 col-md-push-4 text-center">
 				<div class="well">
 					<h1>{{$totalUserCount}}</h1>
-					<h3>twitteraars posten #marathonradio</h3>
+					<h3>twitteraars posten met #marathonradio</h3>
 				</div>
 			</div>
 
 			<div class="col-md-4 col-md-pull-4">
 				<div class="well">
-					<ul class="list-group">
-						<h2 class="text-center" style="margin-bottom:15px;font-weight:bold;">Meeste tweets met #marathonradio</h2>
+					<h2 class="text-center" style="margin-bottom:15px;font-weight:bold;">Meeste tweets met #marathonradio</h2>
+					<ul class="list-group scrollable">
 						@foreach($usersWithMostHashtags as $user)
 							<li class="list-group-item">
-								<a href="https://twitter.com/{{$user->username}}">&commat;{{$user->username}}</a>
+								<a target="_blank" href="https://twitter.com/{{$user->username}}">&commat;{{$user->username}}</a>
 								<span class="badge">{{$user->count}} tweets</span>
 							</li>
 						@endforeach
@@ -58,6 +60,12 @@
 		</div>
 	</div>
 	<!-- /container -->
+
+	<footer>
+		<div class="container text-center">
+			<small>Gemaakt door <a target="_blank" href="https://twitter.com/AlexVanderbist">&commat;AlexVanderbist</a></small>
+		</div>
+	</footer>
 
 	<script src="{{asset("js/app.js")}}"></script>
 
