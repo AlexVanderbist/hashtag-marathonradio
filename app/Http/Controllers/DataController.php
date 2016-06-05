97ac67a4aafedeb603b180e288937009f95fca3c
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-05 18:49:43 +0200

    added person tweet count

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -28,6 +28,39 @@ class DataController extends Controller
 		$tpm = TweetsPerSchedule::all()->last()->num_new_tweets;
 		$tps = $tpm / 60;
 
-		return response()->json(compact('usersWithMostHashtags', 'totalTweetCount', 'totalUserCount', 'tpm', 'tps'));
+		$schedule = [
+			[
+				'name' => 'julie',
+				'start' => '2016-06-04 10:00:00',
+				'stop' => '2016-06-04 18:00:00'
+			],
+			[
+				'name' => 'tom',
+				'start' => '2016-06-04 18:00:00',
+				'stop' => '2016-06-05 08:00:00'
+			],
+			[
+				'name' => 'peter',
+				'start' => '2016-06-05 08:00:00',
+				'stop' => '2016-06-05 18:00:00'
+			],
+			[
+				'name' => 'julie',
+				'start' => '2016-06-05 18:00:00',
+				'stop' => '2016-06-06 08:00:00'
+			]
+		];
+
+		$tweetsPerPerson = [
+			'julie' => 0,
+			'tom' => 0,
+			'peter' => 0
+		];
+
+		foreach ($schedule as $key => $schedule) {
+			$tweetsPerPerson[$schedule['name']] += Tweet::whereBetween('tweeted_at', [$schedule['start'], $schedule['stop']])->get()->count();
+		}
+
+		return response()->json(compact('tweetsPerPerson','usersWithMostHashtags', 'totalTweetCount', 'totalUserCount', 'tpm', 'tps'));
 	}
 }
