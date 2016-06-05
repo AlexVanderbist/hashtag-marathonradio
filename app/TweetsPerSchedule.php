60835d8b78a4553169e721bc9afcdec4d79859fa
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-05 16:12:00 +0200

    added tweets per schedule

--- app/TweetsPerSchedule.php
+++ app/TweetsPerSchedule.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class TweetsPerSchedule extends Model
 {
 	public $timestamps = false;
-	
+
     protected $table = 'tweets_per_schedule';
 
 	protected $fillable = [
-		'num__new_tweets'
+		'num_new_tweets'
 	];
 }
