2778c449c874a85709fa65256ecb2b3f92cf3ddc
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-05 03:02:38 +0200

    added schedule for pulling tweets

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,10 +13,15 @@ class HomeController extends Controller
 {
     public function index() {
 
+		$usersWithMostHashtags = DB::table('tweets')
+					                ->select(DB::raw('username'), DB::raw('count(*) as count'))
+					                ->groupBy('user_id')
+					                ->orderBy('count', 'desc')
+					                ->take(300)
+					                ->get();
 
 
-
-		return view('index', compact('hashtag'));
+		return view('index', compact('usersWithMostHashtags'));
 	}
 
 	private function getTweetsWithTag($tag, $max_id = null){
