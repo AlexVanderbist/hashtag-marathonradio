d058b59da7608511410a6885ebeaebb4526e1e5e
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-06 05:59:11 +0200

    added gen

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-		$schedule->command('tweets:generateoccurences')->everyTenMinutes;
+		$schedule->command('tweets:generateoccurences')->everyTenMinutes();
 
 		$schedule->call(function () {
 
