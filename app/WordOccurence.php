7cc9a2a00a70ee1319c0e128f0f14db85b2d378e
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-10 07:55:22 +0200

    test page

--- app/WordOccurence.php
+++ app/WordOccurence.php
@@ -28,7 +28,7 @@ class WordOccurence extends Model
 		$tweetCount = 0;
 
 		foreach ($tweets as $key => $tweet) {
-			$wordList = self::extract_common_words($tweet->tweet,100);
+			$wordList = self::extract_common_words($tweet->tweet);
 			foreach ($wordList as $word => $count) {
 				if(array_key_exists($word, $allWordsList)) $allWordsList[$word] += $count;
 				else $allWordsList[$word] = $count;
@@ -41,27 +41,30 @@ class WordOccurence extends Model
 		return $allWordsList;
    }
 
-	static function extract_common_words($string, $max_count = 5) {
+	static function extract_common_words($string) {
 		$string = preg_replace('/ss+/i', '', $string);
 		$string = trim($string); // trim the string
-		$string = preg_replace('/[^a-zA-Z -]/', '', $string); // only take alphabet characters, but keep the spaces and dashes too…
+		$string = preg_replace('/[^a-zA-Z\d -]/', '', $string); // only take alphabet characters, but keep the spaces and dashes too…
 		$string = strtolower($string); // make it lowercase
 
 		preg_match_all('/\b.*?\b/i', $string, $match_words);
 		$match_words = $match_words[0];
 
 		foreach ( $match_words as $key => $item ) {
-			if ( $item == '' || in_array(strtolower($item), config('stopwords')) || strlen($item) <= 3 ) {
+			$item = trim($item);
+			if ($item == '' || in_array(strtolower($item), config('stopwords')) || strlen($item) <= 3 ) {
+				//echo $item; // PAS DE STRLEN NOG AAN :(
+				//if($item == "")echo '"' . $item . ','.$key.'"+';
 				unset($match_words[$key]);
 			}
 		}
 
-		$word_count = str_word_count( implode(" ", $match_words) , 1);
-		$frequency = array_count_values($word_count);
-		arsort($frequency);
 
-		//arsort($word_count_arr);
-		$keywords = array_slice($frequency, 0, $max_count);
-		return $keywords;
+		// $word_count = str_word_count( implode(" ", $match_words) , 1);
+		$frequency = array_count_values($match_words);
+		//arsort($frequency)
+
+		//dd($frequency);
+		return $frequency;
 	}
 }
