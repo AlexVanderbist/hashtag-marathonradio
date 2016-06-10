b96b1714b85c39e7d4e2d7aba896ed0814ac0dec
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-10 08:05:35 +0200

    test page

--- app/Console/Commands/GenerateWordOccurences.php
+++ app/Console/Commands/GenerateWordOccurences.php
@@ -3,9 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Tweet;
-use Carbon\Carbon;
-use DB;
 use App\WordOccurence;
 
 class GenerateWordOccurences extends Command
@@ -40,49 +37,14 @@ class GenerateWordOccurences extends Command
      * @return mixed
      */
 	 public function handle() {
- 		$allTweets = Tweet::all();
- 		$allWordsList = [];
-		$tweetCount = 0;
+ 		$occurences = WordOccurences::all();
 
- 		foreach ($allTweets as $key => $tweet) {
- 			$wordList = $this->extract_common_words($tweet->tweet,100);
- 			foreach ($wordList as $word => $count) {
- 				if(array_key_exists($word, $allWordsList)) $allWordsList[$word] += $count;
- 				else $allWordsList[$word] = $count;
- 			}
-			$tweetCount++;
- 		}
-
- 		foreach ($allWordsList as $word => $occurences) {
+ 		foreach ($occurences as $word => $count) {
  			$oldWordOccurence = WordOccurence::firstOrCreate([
  				'word' => $word
  			]);
- 			$oldWordOccurence->update(['occurences' => $occurences]);
+ 			$oldWordOccurence->update(['occurences' => $count]);
  		}
-		echo "\n\rDone: " + $tweetCount + " tweets processed!\n\r";
- 	}
-
- 	function extract_common_words($string, $max_count = 5) {
- 		$string = preg_replace('/ss+/i', '', $string);
- 		$string = trim($string); // trim the string
- 		$string = preg_replace('/[^a-zA-Z -]/', '', $string); // only take alphabet characters, but keep the spaces and dashes too…
- 		$string = strtolower($string); // make it lowercase
-
- 		preg_match_all('/\b.*?\b/i', $string, $match_words);
- 		$match_words = $match_words[0];
-
- 		foreach ( $match_words as $key => $item ) {
- 			if ( $item == '' || in_array(strtolower($item), config('stopwords')) || strlen($item) <= 3 ) {
- 				unset($match_words[$key]);
- 			}
- 		}
-
- 		$word_count = str_word_count( implode(" ", $match_words) , 1);
- 		$frequency = array_count_values($word_count);
- 		arsort($frequency);
-
- 		//arsort($word_count_arr);
- 		$keywords = array_slice($frequency, 0, $max_count);
- 		return $keywords;
+		echo "\n\r\n\rDone: " + count($occurences) + " tweets processed!\n\r\n\r";
  	}
 }
