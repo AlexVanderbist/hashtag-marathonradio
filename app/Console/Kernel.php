e61fa6a7035d703399684f31614a201ac273661a
AlexVanderbist <alex.vanderbist@gmail.com>
2016-06-21 17:36:37 +0200

    stopped hourly occurences

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-		$schedule->command('tweets:generateoccurences')->hourly();
+		//$schedule->command('tweets:generateoccurences')->hourly();
 
 		$schedule->command('tweets:load')->everyMinute();
     }
